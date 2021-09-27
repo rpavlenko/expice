717f6ad78792f2fdc57a343821cb1eb19d972de5
Roman Pavlenko <rpavlenko.mail@gmail.com>
2021-09-27 20:04:06 +0200

    change spinner

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,12 +1,12 @@
 import * as styles from './Loader.module.scss';
+import logo from '../../images/loader.gif';
+
+// https://media2.giphy.com/media/26DMW5ezY0HyXDxiE/giphy.gif?cid=ecf05e478dit1bgj9kf4mjj2nb98a8o1lyo3obxuyg201hm7&rid=giphy.gif&ct=g
 
 export default function Loader() {
   return (
     <div className={styles.loader}>
-      <img
-        className={styles.loaderImage}
-        src="https://media2.giphy.com/media/26DMW5ezY0HyXDxiE/giphy.gif?cid=ecf05e478dit1bgj9kf4mjj2nb98a8o1lyo3obxuyg201hm7&rid=giphy.gif&ct=g"
-      />
+      <img className={styles.loaderImage} src={logo} />
     </div>
   );
 }
