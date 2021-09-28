028995136aeb5e5f85a8d373b4a5b9fced918f20
Roman Pavlenko <rpavlenko.mail@gmail.com>
2021-09-28 23:20:21 +0200

    add download app section with pixel perfect

--- src/components/DownloadApp/DownloadApp.js
+++ src/components/DownloadApp/DownloadApp.js
@@ -9,46 +9,51 @@ export default function DownloadApp() {
   const title = 'Download app for Exciting Deals';
 
   return (
-    <section className={`container ${styles.download}`}>
-      <div className={styles.downloadHeading}>
-        <SectionTitle className="sectionTitle download" sectionTitle={title} />
-        <p className={styles.downloadDescription}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est,
-          adipiscing cursus auctor eget sed phasellus senectus. Ut tellus donec
-          vestibulum tristique leo bibendum in a, tincidunt.
-        </p>
-        <div className="buttonGroup">
-          <a
-            href="https://play.google.com/store"
-            className={styles.downloadButtons}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              className={styles.appImage}
-              src={google}
-              alt="google play logo"
-            />
-          </a>
-          <a
-            href="https://www.apple.com/pl/app-store/"
-            className={styles.downloadButtons}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              className={styles.appImage}
-              src={apple}
-              alt="apple store logo"
-            />
-          </a>
+    <div className={`${styles.downloadWrapper} fullContainer`}>
+      <section className={`container ${styles.download}`}>
+        <div className={styles.downloadHeading}>
+          <SectionTitle
+            className="sectionTitle download"
+            sectionTitle={title}
+          />
+          <p className={styles.downloadDescription}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est,
+            adipiscing cursus auctor eget sed phasellus senectus. Ut tellus
+            donec vestibulum tristique leo bibendum in a, tincidunt.
+          </p>
+          <div className="buttonGroup">
+            <a
+              href="https://play.google.com/store"
+              className={styles.downloadButtons}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className={styles.appImage}
+                src={google}
+                alt="google play logo"
+              />
+            </a>
+            <a
+              href="https://www.apple.com/pl/app-store/"
+              className={styles.downloadButtons}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className={styles.appImage}
+                src={apple}
+                alt="apple store logo"
+              />
+            </a>
+          </div>
         </div>
-      </div>
-      <StaticImage
-        src="../../images/phone.png"
-        alt="Phone Image"
-        className={styles.phoneImage}
-      />
-    </section>
+        <StaticImage
+          className={styles.phoneImage}
+          src="../../images/phone.png"
+          alt="Phone Image"
+        />
+      </section>
+    </div>
   );
 }
