f3cd4190091f84386a0afbba0fd0d340d97f8254
Roman Pavlenko <rpavlenko.mail@gmail.com>
2023-01-31 23:51:19 +0100

    feat: add blog post page with styles

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,6 +3,8 @@ import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import Layout from '../components/Layout/Layout';
 import Footer from '../components/Footer/Footer';
 
+import * as styles from './blog-post.module.scss';
+
 export default function BlogPost({ data }) {
   const post = data.allWpPost.nodes[0];
   console.log(post);
@@ -13,10 +15,20 @@ export default function BlogPost({ data }) {
   return (
     <>
       <Layout blogList={'blogList'}>
-        <div>
-          <h1>{post.title}</h1>
-          <div dangerouslySetInnerHTML={{ __html: post.content }} />
-          <GatsbyImage image={featuredImage} alt={`${post.title} image`} />
+        <div className={styles.postContainer}>
+          <h1 className={styles.postTitle}>{post.title}</h1>
+          <div
+            className={styles.postDate}
+            dangerouslySetInnerHTML={{
+              __html: post.date,
+            }}
+          />
+          <div
+            className={styles.postContent}
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
+
+          {/* <GatsbyImage image={featuredImage} alt={`${post.title} image`} /> */}
         </div>
       </Layout>
       <Footer />
@@ -29,6 +41,7 @@ export const query = graphql`
       nodes {
         title
         content
+        date(formatString: "DD MMMM, YYYY", locale: "en")
         featuredImage {
           node {
             gatsbyImage(height: 300, width: 300)
