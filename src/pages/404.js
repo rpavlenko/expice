23452f8f84031236fdaabe0326a7ee9ea6327346
Roman Pavlenko <rpavlenko.mail@gmail.com>
2023-02-07 22:12:11 +0100

    feat: add image to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,16 @@
+import { Link } from 'gatsby';
 export default function NotFound() {
   return (
-    <div>
-      <h2>404 Page</h2>
-      <p>Sorry, page doesnt exists</p>
-    </div>
+    <>
+      <div className="page404">
+        <p className="titlePage404">Sorry, page doesn't exists</p>
+        <Link className="linkPage404" to="/">
+          Go back
+        </Link>
+        <a className="imageLicensePage404" href="http://www.freepik.com">
+          Designed by Freepik
+        </a>
+      </div>
+    </>
   );
 }
