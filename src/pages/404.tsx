1b0419d4cf5b02975dd319e77a84b758c80c5b69
Roman Pavlenko <rpavlenko.mail@gmail.com>
2023-11-19 19:02:19 +0100

    fix: change 404 page and add static image

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,11 +5,11 @@ export default function NotFound() {
   return (
     <>
       <div className="page404">
-        <p className="titlePage404">Sorry, page doesn't exists</p>
-        <StaticImage src="../images/404.jpg" alt="404 image" />
         <Link className="linkPage404" to="/">
           Go back
         </Link>
+        <StaticImage src="../images/404.jpg" alt="404 image" />
+        <br></br>
         <a className="imageLicensePage404" href="http://www.freepik.com">
           Designed by Freepik
         </a>
