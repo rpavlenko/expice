525325747542b60d2d383f3a906b59135afa2fe4
Roman Pavlenko <rpavlenko.mail@gmail.com>
2023-11-19 18:56:57 +0100

    fix: try to use static image on 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,12 @@
 import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 
 export default function NotFound() {
   return (
     <>
       <div className="page404">
         <p className="titlePage404">Sorry, page doesn't exists</p>
+        <StaticImage src="../images/404.jpg" alt="404 image" />
         <Link className="linkPage404" to="/">
           Go back
         </Link>
